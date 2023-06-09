82d45ff1b6cdfbe36eac67a4cf775019e3fcddc3
Denil Chungath <denilct@sharechat.co>
2023-06-09 14:36:07 +0530

    Initial Commit

---
+++ ASCReviews/ASCReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCReviewsApp.swift
+//  ASCReviews
+//
+//  Created by Denil C T on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ASCReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
